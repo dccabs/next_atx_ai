f0b491dd2c25a381d546f4139a8181f7f4248abc
Dan Cabaniss <dccabs@gmail.com>
2023-11-09 12:06:38 -0600

    Basic Chat example

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,39 +1,193 @@
 'use client'
+import { useState } from 'react'
+import BasicChat from './components/BasicChat'
+import Tabs from '@/app/components/ui/Tabs'
 
-import Input from './components/input'
+export default function Chat() {
+  const [tabs, setTabs] = useState([
+    {
+      id: 'instructions',
+      name: 'Instructions',
+      current: true,
+      onClick: () => {
+        handleTabSelect('instructions')
+      }
+    },
+    {
+      id: 'open-ai-demo',
+      name: 'Open AI Demo',
+      current: false,
+      onClick: () => {
+        handleTabSelect('open-ai-demo')
+      }
+    }
+    // {
+    //   id: 'replicate-demo',
+    //   name: 'Replicate Demo',
+    //   current: false,
+    //   onClick: () => {
+    //     handleTabSelect('replicate-demo')
+    //   }
+    // }
+  ])
 
-export default function Example() {
+  const handleTabSelect = (id) => {
+    const tabsCopy = [...tabs]
+    const activeTab = tabsCopy.find((tab) => tab.current)
+    const tab = tabsCopy.find((tab) => tab.id === id)
+    tab.current = true
+    activeTab.current = false
+    setTabs(tabsCopy)
+  }
+
+  const activeTab = tabs.find((tab) => tab.current)
+
+  console.log('activeTab', activeTab)
+
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto lg:mx-0 space-y-6">
-          <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Vercel AI SDK
-          </h2>
-          <h4 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-            Chat Example
-          </h4>
-          <div className="max-w-2xl">
-            <div className="prose prose-slate">
-              <pre>
-                <code className="language-js">
-                  {`
-module.exports = {
-  purge: [],
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}`}
-                </code>
-              </pre>
-            </div>
-            <div>
-              <Input />
+    <div className='bg-white py-24 sm:py-32'>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8 space-y-8'>
+        <div className='max-w-7xl'>
+          <div className='mx-auto lg:mx-0 space-y-4'>
+            <h2 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+              Basic Chat Example
+            </h2>
+            <div className='space-y-4'>
+              <p>
+                The vercel AI makes it simple to re-create a basic chat GPT
+                clone using the{' '}
+                <span className='text-cyan-600'>Vercel AI SDK</span>
+              </p>
             </div>
           </div>
         </div>
+        <Tabs tabs={tabs} />
+        {activeTab.id === 'instructions' && (
+          <>
+            <div className='max-w-7xl'>
+              <div className='space-y-4'>
+                <h5 className='text-bold text-2xl'>
+                  First we're going to create a new React Component called
+                  BasicChat.tsx
+                </h5>
+                <p>In this component we'll want to do a couple of things</p>
+                <ul className='list-disc space-y-4 ml-16'>
+                  <li>
+                    Use "use client" as this will need to be a client rendered
+                    component. We'll be streaming data directly from the OpenAI
+                    Api
+                  </li>
+                  <li>
+                    We'll be using the useChat hook provided by the Vercel AI
+                    SDK which gives us some nice out of the box functionality
+                  </li>
+                </ul>
+                <div className='max-w-7xl'>
+                  <div className='prose prose-slate max-w-7xl'>
+                    <pre>
+                      <code className='language-js'>
+                        {`'use client'
+import { useChat } from 'ai/react'
+
+export default function BasicChat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    // This will default to ./api/chat - but you can point it elsewhere
+    // api: './api/chat'
+  })
+  return (
+    <div className='flex flex-col w-full max-w-7xl mx-auto stretch space-y-4'>
+      {messages.length > 0
+        ? messages.map((m) => (
+          <div key='{m.id}' className='whitespace-pre-wrap'>
+            <div>
+              {m.role === 'user' ? (
+                <div className='p-2 bg-rose-200 text-gray-900 font-semibold rounded mb-4'>
+                  User
+                </div>
+              ) : (
+                <div className='p-2 bg-emerald-200 text-gray-900 font-semibold rounded mb-4'>
+                  AI
+                </div>
+              )}
+            </div>
+            <div className='px-8 text-gray-900 font-semibold'>
+              {m.content}
+            </div>
+          </div>
+        ))
+        : null}
+
+      <form onSubmit='{handleSubmit}'>
+        <input
+          className='w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl'
+          value='{input}'
+          placeholder='Say something...'
+          onChange='{handleInputChange}'
+        />
+      </form>
+    </div>
+  )
+}`}
+                      </code>
+                    </pre>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='max-w-7xl'>
+              <div className='space-y-4'>
+                <h5 className='text-bold text-2xl'>
+                  Next: Create an edge function
+                </h5>
+                <ul className='list-disc space-y-4 ml-16'>
+                  <li>
+                    Create a .ts edge function in your api directory
+                  </li>
+                  <li>
+                    We will use this function to stream the data from the
+                    OpenAI API.
+                  </li>
+                </ul>
+                <div className='max-w-7xl'>
+                  <div className='prose prose-slate max-w-7xl'>
+                    <pre>
+                      <code className='language-js'>
+                        {`import OpenAI from 'openai'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || ''
+})
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+  // Extract the \`prompt\` from the body of the request
+  const { messages } = await req.json()
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4',
+    stream: true,
+    messages: messages
+  })
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+  // Respond with the stream
+  return new StreamingTextResponse(stream)
+}`}
+                      </code>
+                    </pre>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+        {activeTab.id === 'open-ai-demo' && <BasicChat />}
       </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,85 @@
 'use client'
 
-import Input from './components/input'
+import Link from 'next/link'
+
+const Links = [
+  { href: '/chat', label: 'Basic Chat Example' },
+  { href: '/chat', label: 'Open AI Functions Example' },
+  { href: '/chat', label: 'Basic Langchain Chat Example' },
+  { href: '/chat', label: 'Lanchain Retrieval Example' },
+  { href: '/chat', label: 'Serp (Search Engine API) Example' },
+  { href: '/chat', label: 'Vercel AI SDK Playground' },
+]
 
 export default function Example() {
   return (
-    <div className='bg-white py-24 sm:py-32'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto lg:mx-0 space-y-6'>
-          <h2 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-            Next.js ATX Meetup
-          </h2>
-          <h4 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl'>
-            Vercel AI SDK
-          </h4>
-          <div className='max-w-2xl'>
-            <ul className='list-decimal space-y-4 text-xl font-semibold ml-8'>
-              <li>What is the Vercel AI SDK?</li>
-              <li>Streaming Data - Why do this? How do we do it?</li>
-              <li>Open AI Function Calling - Turning unstructured data into structured data</li>
-              <li>Langchain - What is langchain? How can we use lanchain in the vercel AI SDK?</li>
-              <li>Vector DB's - What is the purpose of a vector DB? How can we use one in Next.js (supabase)</li>
-            </ul>
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto lg:mx-0 space-y-6">
+          <div className="space-y-8">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Next.js ATX Meetup
+            </h2>
+            <h4 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+              Vercel AI SDK
+            </h4>
+            <div className="space-y-4">
+              <p>
+                First add the following dependencies to try out the examples
+                below:
+              </p>
+              <div className="max-w-2xl">
+                <div className="prose prose-slate">
+                  <pre>
+                    <code className="language-js">
+                      {`yarn add ai
+yarn add openai`}
+                    </code>
+                  </pre>
+                </div>
+              </div>
+            </div>
+
+            <div className="space-y-4">
+              <p>
+                If you want to use Open AI, or Replicate, you'll need to get an
+                API key from{' '}
+                <Link
+                  target="_blank"
+                  className="underline text-cyan-600"
+                  href="https://openai.com/"
+                >
+                  https://openai.com/
+                </Link>
+                :
+              </p>
+              <div className="max-w-2xl">
+                <div className="prose prose-slate">
+                  <pre>
+                    <code className="language-js">
+                      {`Add to your .env file:
+                    
+OPENAI_API_KEY=sk-*******`}
+                    </code>
+                  </pre>
+                </div>
+              </div>
+            </div>
+            <div className="max-w-2xl space-y-4">
+              <h4 className="font-bold text-3xl">Examples:</h4>
+              <ul className="list-decimal space-y-4 text-xl font-semibold ml-8">
+                {Links.map(({ href, label }) => (
+                  <li key={`${href}${label}`}>
+                    <Link
+                      className="text-cyan-600 hover:text-cyan:800 hover:underline"
+                      href={href}
+                    >
+                      {label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
